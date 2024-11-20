14c97a34938ba4f2fe261da2cb3d65df5dc12bf9
wemersonmatheuss <wemersonmthdev@gmail.com>
2024-11-19 21:41:28 -0300

    adicionando setters

--- src/locacao_imoveis/Cliente.java
+++ src/locacao_imoveis/Cliente.java
@@ -17,11 +17,23 @@ public class Cliente {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getCpf() {
         return cpf;
     }
 
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
     public String getTelefone() {
         return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
     }
 }
