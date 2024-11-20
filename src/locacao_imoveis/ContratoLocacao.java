38544f2ce015de4440f08c23aa578c41bf9c0682
wemersonmatheuss <wemersonmthdev@gmail.com>
2024-11-19 23:03:31 -0300

    criação da classe contrato e implementação dos atributos e dos getters e setters

---
+++ src/locacao_imoveis/ContratoLocacao.java
@@ -0,0 +1,54 @@
+package locacao_imoveis;
+
+import java.time.LocalDate;
+
+public class ContratoLocacao {
+
+    private Cliente cliente;
+    private Imovel imovel;
+    private LocalDate dataInicio;
+    private LocalDate dataTermino;
+    private double valorMensal;
+    private boolean ativo;
+
+    public ContratoLocacao(Cliente cliente, Imovel imovel, LocalDate dataInicio, LocalDate dataTermino, double valorMensal) {
+        this.cliente = cliente;
+        this.imovel = imovel;
+        this.dataInicio = dataInicio;
+        this.dataTermino = dataTermino;
+        this.valorMensal = valorMensal;
+        this.ativo = true;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Imovel getImovel() {
+        return imovel;
+    }
+
+    public void setImovel(Imovel imovel) {
+        this.imovel = imovel;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataTermino() {
+        return dataTermino;
+    }
+
+    public void setDataTermino(LocalDate dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+}
