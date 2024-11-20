19f1878e2fe21bd5d476bd62d2c51236e155e8b7
wemersonmatheuss <wemersonmthdev@gmail.com>
2024-11-19 22:43:58 -0300

    criação da classe imovel junto com os atributos e os getters e setters

---
+++ src/locacao_imoveis/Imovel.java
@@ -0,0 +1,49 @@
+package locacao_imoveis;
+
+public class Imovel {
+
+    private String endereco;
+    private double valorAluguel;
+    private String tipo; // casa, ap, etc...
+    private boolean disponivel;
+
+    public Imovel(String endereco, double valorAluguel, String tipo) {
+        this.endereco = endereco;
+        this.valorAluguel = valorAluguel;
+        this.tipo = tipo;
+        this.disponivel = true;
+
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
